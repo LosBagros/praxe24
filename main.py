54722dac3202ac084a5f46ef03695c546e70a311
Bagros <tom@bagros.eu>
2024-04-15 13:52:24 +0200

    uzivatele

---
+++ main.py
@@ -0,0 +1,35 @@
+
+pole_cisel = [
+    1, 2, 3, 4, 5
+]
+uzivatele = [
+    {
+        "id": 1,
+        "name": "Tom",
+        "img_url": "https://placehold.co/200",
+        "links": ["https://placehold.co/200",
+                  "https://placehold.co/200",
+                  "https://placehold.co/200"]
+
+    },
+    {
+        "id": 2,
+        "name": "Jakub",
+        "img_url": "https://placehold.co/200",
+        "links": ["https://placehold.co/200",
+                  "https://placehold.co/200",
+                  "https://placehold.co/200"]
+
+    },
+    {
+        "id": 3,
+        "name": "Pepa",
+        "img_url": "https://placehold.co/200",
+        "links": ["https://placehold.co/200",
+                  "https://placehold.co/200",
+                  "https://placehold.co/200"]
+
+    }
+]
+
+print(uzivatele[0]["links"][0])
